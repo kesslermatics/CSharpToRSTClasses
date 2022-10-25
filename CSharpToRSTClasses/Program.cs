4b91d7a8d05723554a20e94acf7a4a717e7a14ac
kesslermatics <Robby9903@gmail.com>
2022-10-25 21:13:17 +0200

    Added TestCases

---
+++ CSharpToRSTClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpToRSTClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the class path");
+            string classPath = Console.ReadLine();
+
+            string[] cSharpClass = File.ReadAllLines(classPath);
+
+            while (true);
+
+        }
+    }
+}
---
+++ CSharpToRSTClasses/TestCSharpClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToRSTClasses
+{
+    class TestCSharpClass
+    {
+        private string Property1;
+        private List<string> Property2;
+
+        public int Number1;
+
+        public string Property12
+        {
+            get => Property1;
+
+            set => Property1 = value;
+        }
+
+        TestCSharpClass()
+        {
+
+        }
+
+        TestCSharpClass(string testString1, int testInt1)
+        {
+
+        }
+
+        private void PrivateTestMethodWithTwoArgumentsAndNoReturnValue(string testString1, int testInt1)
+        {
+
+        }
+
+        private int PrivateTestMethodWithTwoArgumentsAndReturnValue(string testString1, int testInt1)
+        {
+            return 1;
+        }
+
+        public void PublicTestMethodWithTwoArgumentsAndNoReturnValue(string testString1, int testInt1)
+        {
+            
+        }
+
+        public int PublicTestMethodWithTwoArgumentsAndReturnValue(string testString1, int testInt1)
+        {
+            return 1;
+        }
+
+
+
+    }
+}
